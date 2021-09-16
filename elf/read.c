916c9f8582028f15e1cda5d5a4d8183d9c1b1664
chemchemnaresh <nareshchemchem35@gmail.com>
2021-09-16 17:39:59 +0530

    arm

--- elf/read.c
+++ elf/read.c
@@ -1,19 +1,22 @@
-
-
+#include<stdio.h>
+#include<unistd.h>
+#include<stdint.h>
+#include<stdlib.h>
+#include<elf.h>
 
 int main(int argc,char **argv)
 {
 	FILE* Elffile = NULL;
-	char* Sectnames = NULL;
+	char* SectNames = NULL;
 	Elf64_Ehdr elfHdr;
-	Elf_Shdr sectHdr;
-	unit32_t idx;
+	Elf64_Shdr sectHdr;
+	uint32_t idx;
 
 	if(argc != 2){
 		printf("usage: %s <ELF_FILE>\n",argv[0]);
 		exit(1);
 	}
-	if((ELFfile = fopen(argv[1], "r")) = NULL) {
+	if((Elffile = fopen(argv[1], "r")) == NULL) {
 		perror("[E] Error opening file:");
 		exit(1);
 	}
@@ -23,21 +26,21 @@ int main(int argc,char **argv)
 	fseek(Elffile,elfHdr.e_shoff + elfHdr.e_shstrndx * sizeof(sectHdr), SEEK_SET);
 	fread(&sectHdr, 1, sizeof(sectHdr), Elffile);
 
-	SectNames =malloc(sectHdr.sh_size);
-	fseek(Elffile, elfHdr.e_shoff + idx * sizeof(sectHdr), SEEK_SET);
-	fread(&sectHdr, 1, sizeof(secHdr), Elffile);
+	SectNames = malloc(sectHdr.sh_size);
+	fseek(Elffile, sectHdr.sh_offset,SEEK_SET);
+	fread(SectNames, 1 ,sectHdr.sh_size,Elffile);
 
 	/* read all section headers */
 	for (idx = 0; idx < elfHdr.e_shnum; idx++)
 	{
 		const char* name = "";
 
-		fseek9Elffile, elfHdr.e_shoff + idx * sizeof (sectHdr), SEEK_SET);
+		fseek(Elffile, elfHdr.e_shoff + idx * sizeof (sectHdr), SEEK_SET);
 		fread(&sectHdr, 1, sizeof(sectHdr), Elffile);
 
 	/* print section names */
 	if (sectHdr.sh_name);
-	name = SectNames + secHdr.sh_name;
+	name = SectNames + sectHdr.sh_name;
 	printf("%2u %s\n", idx, name);
 	}
 	return 0;
